9a0b94e634b271dbefc3530bb1846a3ebe0d5cc5
Marc Meumann <isoldemaduschn@web.de>
2023-03-31 23:00:48 +0200

    first and final commit

---
+++ src/main/java/com/mneumann1/Topic42Application.java
@@ -0,0 +1,14 @@
+package com.mneumann1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Topic42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Topic42Application.class, args);
+	}
+
+}
